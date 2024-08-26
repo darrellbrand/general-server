02f64a66e27be6cefc9f9f454fdef735bd16600f
dj <darrellbrand@gmail.com>
2024-08-26 06:59:45 -0700

    wip

--- src/main/java/com/djf/generalserver/GeneralServerApplication.java
+++ src/main/java/com/djf/generalserver/GeneralServerApplication.java
@@ -2,13 +2,9 @@ package com.djf.generalserver;
 
 import com.djf.generalserver.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-@EnableJpaRepositories("com.djf.generalserver.repository")
+
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
 public class GeneralServerApplication {
