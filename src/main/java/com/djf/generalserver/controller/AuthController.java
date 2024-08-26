de8a875d56cd359d4a743a61d305c684ff2849de
dj <darrellbrand@gmail.com>
2024-08-26 03:49:49 -0700

    wip

--- src/main/java/com/djf/generalserver/controller/AuthController.java
+++ src/main/java/com/djf/generalserver/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.djf.generalserver.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class AuthController {
     }
 
 
-    @PostMapping("/token")
+    @GetMapping("/token")
     public String token(Authentication authentication) {
         LOG.debug("token requested for user: {}", authentication.getName());
 
