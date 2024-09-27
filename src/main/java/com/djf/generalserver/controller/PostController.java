276f0eca2eda3ea3ce854f8a7be046c3cf49489d
dj <darrellbrand@gmail.com>
2024-09-27 16:11:15 -0700

    wip

--- src/main/java/com/djf/generalserver/controller/PostController.java
+++ src/main/java/com/djf/generalserver/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -26,9 +27,17 @@ public class PostController {
     public ResponseEntity<List<Post>> findAll() {
         return new ResponseEntity<>(repositoryService.findAll(), HttpStatus.OK);
     }
+
     @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @PostMapping("/save")
     public ResponseEntity<Post> savePost(@RequestBody Post post) {
         return new ResponseEntity<>(repositoryService.savePost(post), HttpStatus.CREATED);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+    @GetMapping("/getPost")
+    public ResponseEntity<Post> getPost(@RequestParam String id) {
+        Optional<Post> ret = repositoryService.getPost(Integer.parseInt(id));
+        return ret.map(post -> new ResponseEntity<>(post, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 }
--- src/main/java/com/djf/generalserver/service/RepositoryService.java
+++ src/main/java/com/djf/generalserver/service/RepositoryService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RepositoryService {
@@ -21,7 +22,9 @@ public class RepositoryService {
     public List<Post> findAll() {
         return postRepository.findAll();
     }
-
+    public Optional<Post> getPost(int id) {
+        return postRepository.findById(id);
+    }
     public Post savePost(Post post) {
        return  postRepository.save(post);
     }
